fb89f74ecadf81bbc740125ec6039d8b6c94dce6
gabrielperezx <gabrieloctavioperez@gmail.com>
2025-09-15 01:46:52 -0400

    Agregar menu fijo y carousel de la seccion Hero

---
+++ src/js/app.js
@@ -0,0 +1,83 @@
+(function () {
+    /*----- ----- ----- ----- -----
+	# Funciones
+	----- ----- ----- ----- -----*/
+
+    const GliderCreator = () => {
+        const gliderConfig3Slides = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            scrollLock: true,
+
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        };
+
+        const gliderConfig1SlidesBase = {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            scrollLock: true,
+        };
+
+        document.querySelectorAll('.carousel__items').forEach((carousel) => {
+            const container = carousel.closest('.carousel');
+            const prevButton = container.querySelector('.carousel__anterior');
+            const nextButton = container.querySelector('.carousel__siguiente');
+            const dots = container.querySelector('.carousel__indicadores');
+
+            new Glider(carousel, {
+                ...gliderConfig3Slides,
+                arrows: {
+                    prev: prevButton,
+                    next: nextButton,
+                },
+                dots: dots,
+            });
+        });
+
+        document.querySelectorAll('.carousel__lista-1').forEach((carousel) => {
+            const container = carousel.closest('.carousel');
+            const prevButton = container.querySelector('.carousel__anterior');
+            const nextButton = container.querySelector('.carousel__siguiente');
+            const dots = container.querySelector('.carousel__indicadores');
+
+            new Glider(carousel, {
+                ...gliderConfig1SlidesBase,
+                arrows: {
+                    prev: prevButton,
+                    next: nextButton,
+                },
+                dots: dots,
+            });
+        });
+    };
+
+    const MenuEstatico = () => {
+        const navbar = document.getElementById('mainNavbar');
+        if (document.body.contains(navbar)) {
+            window.addEventListener('scroll', () => {
+                if (window.scrollY > 120) {
+                    navbar.classList.remove('d-none');
+                } else {
+                    navbar.classList.add('d-none');
+                }
+            });
+        }
+    };
+
+    /*----- ----- ----- ----- -----
+	# Declaraciones
+	----- ----- ----- ----- -----*/
+
+    window.addEventListener('load', () => {
+        GliderCreator();
+        MenuEstatico();
+    });
+})();
